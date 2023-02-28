b876dcb7075fa866fdabd097b454cf0e49352c26
meaculpv <olzhabaevbatyr@gmail.com>
2023-03-01 01:02:36 +0600

    Style User.js and Sidebar.js

---
+++ src/components/Sidebar.js
@@ -0,0 +1,30 @@
+import { Drawer, List, ListItem, ListItemButton, ListItemText } from "@mui/material";
+import { React } from "react";
+
+const drawerWidth = 240;
+
+function Header({ children }) {
+    
+    return (
+        <Drawer 
+        className="Sidebar" 
+        sx={{
+            width: drawerWidth,
+            flexShrink: 0,
+            '& .MuiDrawer-paper': {
+                width: drawerWidth,
+                boxSizing: 'border-box',
+            },
+        }}
+        variant="permanent"
+        anchor="left"
+        >
+            <List>
+                {children}
+            </List> 
+        </Drawer>
+    )
+
+}
+
+export default Header
--- src/components/User.js
+++ src/components/User.js
@@ -1,10 +1,46 @@
+import { Avatar, Link, Typography } from "@mui/material";
+import { Box } from "@mui/system";
 import { React } from "react";
+import avatar from "./assets/avatar.png"
 
 function User() {
     
     return (
         <div className="User">
-            User
+            <Box
+                sx={{
+                    borderBottom: 1,
+                    display: 'flex',
+                    justifyContent: 'space-around',
+                    borderColor: 'primary.main',
+                    alignItems: 'center',
+                }}
+            >
+                <Avatar
+                className="avatar"
+                alt="user-avatar" 
+                src={avatar} 
+                sx={{
+                    width: 56,
+                    height: 56,
+                }} />
+                <Box
+                >
+                    <Typography
+                        variant="h6"
+                    >
+                        Eoin Duke
+                    </Typography>
+                    <Link
+                        href="#"
+                        sx={{
+                        textDecoration: 'none',
+                        display: 'flex',
+                        justifyContent: 'flex-end'
+                    }}
+                    >Logout</Link>
+                </Box>
+            </Box>
         </div>
     )
 
